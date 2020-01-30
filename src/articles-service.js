482f65e87a64564abae4d5855a2efe5f1d896353
Scott Lingner <slingner@gmail.com>
2020-01-30 14:34:20 -0800

    getById scope issue

--- src/articles-service.js
+++ src/articles-service.js
@@ -1,9 +1,8 @@
+
 //service means it serves as we are asking it to serve something for us
-
-
 const ArticlesService = {
   //function inside our service object
-  getAllArticles: function (knex) {     //shorthand is getAllArticles() {}, this is the first key!
+  getAllArticles(knex) {     //shorthand is getAllArticles() {}, this is the first key!
     return knex.select('*').from('blogful_articles');   
   },
   insertArticle(knex, newArticle) {
@@ -13,20 +12,20 @@ const ArticlesService = {
       .returning('*')
       .then(rows => {
         return rows[0];
-      });
+      })
   }, 
-  getById(knex, id) {
-    return knex.from('blogful_articles').select('*').where('id', id).first();
-  },
+   getById(knex, id) {
+      return knex('blogful_articles').select('*').where('id', id).first()
+   },
   deleteArticle(knex, id) {
     return knex('blogful_articles')
       .where({ id })
-      .delete();
+      .delete()
   },
   updateArticle(knex, id, newArticleFields) {
     return knex('blogful_articles')
       .where({ id })
-      .update(newArticleFields);
+      .update(newArticleFields)
   },
 };
 
--- test/articles-service.spec.js
+++ test/articles-service.spec.js
@@ -31,26 +31,27 @@ describe('Articles service object', function () {
   let db;
   
   before(() => {
+    console.log(process.env.TEST_DB_URL)
     db = knex({
       client: 'pg',
       connection: process.env.TEST_DB_URL,
     });
   });
 
+  before(() => db('blogful_articles').truncate());
+
+  before(() => db('blogful_articles').insert(testArticles));
   
   after(() => db.destroy());
 
-  before(() => db('blogful_articles').truncate());
-
   afterEach(() => db('blogful_articles').truncate());
 
-
-  context('Given "blogful_articles" has data', () => {
-    before(() => {
-      return db
-        .into('blogful_articles')
-        .insert(testArticles);
-    });
+  describe('Given "blogful_articles" has data', () => {
+    // beforeEach(() => {
+    //   return db
+    //     .into('blogful_articles')
+    //     .insert(testArticles);
+    // })
     
     it('getAllArticles() resolves all articles from "blogful_articles" table', () => {
       //test here
@@ -62,6 +63,49 @@ describe('Articles service object', function () {
                      })))
         })
     })
+  
+    it(`getById() resolves an article by id from 'blogful_articles' table`, () => {
+      const thirdId = 3
+      const thirdTestArticle = testArticles[thirdId - 1]
+      return ArticlesService.getById(db, thirdId)
+        .then(actual => {
+          expect(actual).to.eql({
+            id: thirdId,
+            title: thirdTestArticle.title,
+            content: thirdTestArticle.content,
+            date_published: thirdTestArticle.date_published,
+          })
+        })
+    });
+
+    it(`deleteArticle() removes an article by id from 'blogful_articles' table`, () => {
+        const articleId = 3
+        return ArticlesService.deleteArticle(db, articleId)
+          .then(() => ArticlesService.getAllArticles(db))
+          .then(allArticles => {
+            // copy the test articles array without the "deleted" article
+            const expected = testArticles.filter(article => article.id !== articleId)
+            expect(allArticles).to.eql(expected)
+          })
+    });
+
+    it(`updateArticle() updates an article from the 'blogful_articles' table`, () => {
+        const idOfArticleToUpdate = 3
+        const newArticleData = {
+          title: 'updated title',
+          content: 'updated content',
+          date_published: new Date(),
+        }
+        return ArticlesService.updateArticle(db, idOfArticleToUpdate, newArticleData)
+          .then(() => ArticlesService.getById(db, idOfArticleToUpdate))
+          .then(article => {
+            expect(article).to.eql({
+              id: idOfArticleToUpdate,
+              ...newArticleData,
+            })
+          })
+      });
+
   });
 
   context(`Given 'blogful_articles' has no data`, () => {
@@ -72,8 +116,21 @@ describe('Articles service object', function () {
          })
      })
    })
+
+   it(`insertArticle() inserts a new article and resolves the new article with an id `, () => {
+    const newArticle = {
+        title: 'Test new title',
+        content: 'Test new content',
+        date_published: new Date('2020-01-01T00:00:00.000Z'),
+      }
+      return ArticlesService.insertArticle(db, newArticle)
+        .then(actual => {
+          expect(actual).to.eql({
+            id: 1,
+            title: newArticle.title,
+            content: newArticle.content,
+            date_published: newArticle.date_published,
+          })
+        })
+    })
 });
-
-  
- 
-
